54ad0a2486ff9a724354c5e7aa75474039278638
getcerts1 <barrowtech1999@gmail.com>
2025-07-23 17:12:32 +0100

    simple count web app init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+
+@app.route("/count")
+def count_page():
+    return render_template("count.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
